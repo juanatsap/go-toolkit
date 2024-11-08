05ea4fbcb15324b4b939ba1934b7757d7212a5e4
juanatsap <juanatsap@gmail.com>
2024-11-08 17:48:35 +0000

    Added json functionality

--- tools.go
+++ tools.go
@@ -1,7 +1,9 @@
 package toolkit
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -20,8 +22,16 @@ const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 // Tools is the type we use to instantiate this module. Any variable of this
 // type will have access to all the methods with the receiver *Tools
 type Tools struct {
-	MaxFileSize      int
-	AllowedFileTypes []string
+	MaxFileSize        int
+	AllowedFileTypes   []string
+	MaxJSONSize        int
+	AllowUnknownFields bool
+}
+
+type JSONResponse struct {
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 // RandomString returns a string of random characters of length n
@@ -199,4 +209,141 @@ func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, p, fi
 	// We want to download the file directly to the client, so we need to set the Content-Disposition header
 	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
 	http.ServeFile(w, r, fp)
+}
+
+// ReadJSON tries to read the body of a request and converts it into JSON.
+// If there is an error, we write the error in the response and return a 400 status code.
+func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+
+	maxBytes := 1024 * 1024 // 1MB
+	if t.MaxFileSize > 0 {
+		maxBytes = t.MaxFileSize
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	if !t.AllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
+	err := dec.Decode(data)
+
+	if err != nil {
+
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+
+		case errors.As(err, &invalidUnmarshalError):
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", invalidUnmarshalError.Type.Bits())
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+
+		case errors.As(err, &invalidUnmarshalError):
+			return fmt.Errorf("error unmarshalling JSON: %s", err.Error())
+
+		default:
+			return err
+
+		}
+	}
+	// It will try to decode more JSON from that fail
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+	return nil
+}
+
+// WriteJSON tries to write the response as JSON.
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Error JSON takes an error and a status code, and writes a JOSN error message to the client
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+
+	statusCode := http.StatusBadRequest
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+	payload := JSONResponse{
+		Error:   true,
+		Message: err.Error(),
+	}
+	return t.WriteJSON(w, statusCode, payload)
+}
+
+// PushJSONToRemote pushes arbitrary JSON data to a remote endpoint and returns the response, status code, and error if any
+// The final parameter is an optional http client. If none is specified, we use the standard http.Client
+func (t *Tools) PushJSONToRemote(uri string, data interface{}, client ...*http.Client) (*http.Response, int, error) {
+	// create json
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// checks for custom http client
+	httpClient := &http.Client{}
+
+	if len(client) > 0 {
+		httpClient = client[0]
+	}
+
+	// create request
+	req, err := http.NewRequest("POST", uri, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, 0, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// send request
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// !!!
+	defer resp.Body.Close()
+
+	// send response back
+	return resp, resp.StatusCode, nil
 }
--- tools_test.go
+++ tools_test.go
@@ -1,6 +1,9 @@
 package toolkit
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"image"
 	"image/png"
@@ -10,6 +13,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"sync"
 	"testing"
 )
@@ -36,6 +40,43 @@ var slugsTests = []struct {
 	{name: "underscore", s: "_", expected: "", errorExpected: true},
 	{name: "two words", s: "abc def", expected: "abc-def", errorExpected: false},
 	{name: "two words with spaces", s: "abc def ghi", expected: "abc-def-ghi", errorExpected: false},
+}
+
+var jsonTests = []struct {
+	name          string
+	json          string
+	error         bool
+	errorMessage  string
+	maxSize       int
+	allowUnknown  bool
+	errorExpected bool
+}{
+	{name: "good JSON", json: `{"foo": "bar"}`, errorExpected: false, maxSize: 1024, allowUnknown: false},
+	{name: "badly formated JSON", json: `{foo": "bar"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "incorrect type", json: `{"foo": 3.14159}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "two json files", json: `{"foo": "1"}{"alpha": "2"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "empty body", json: ``, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "missing closing brace", json: `{"foo": "bar`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "missing colon", json: `{"foo" "bar"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "missing comma and colon", json: `{"foo" "bar" "alpha" "beta"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "missing comma", json: `{"foo": "bar" "alpha": "beta"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "extra comma", json: `{"foo": "bar",`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "not json", json: `Hello, World!`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "unknown field in JSON", json: `{"foo": "bar", "alpha": "beta"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "allow unknown fields", json: `{"foo": "bar", "alpha": "beta"}`, errorExpected: false, maxSize: 1024, allowUnknown: true},
+	{name: "max size", json: `{"foo": "bar", "alpha": "beta"}`, errorExpected: true, maxSize: 5, allowUnknown: false},
+	{name: "missing field name", json: `{:"bar", "alpha": "beta"}`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+	{name: "file size too large", json: `{"foo": "bar"}`, errorExpected: true, maxSize: 5, allowUnknown: false},
+	{name: "not json", json: `Hello, World!`, errorExpected: true, maxSize: 1024, allowUnknown: false},
+}
+
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{Transport: fn}
 }
 
 func TestTools_RandomString(t *testing.T) {
@@ -233,5 +274,133 @@ func TestTools_DownloadStaticFile(t *testing.T) {
 	_, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Error(err)
+	}
+}
+
+func TestTools_ReadJSON(t *testing.T) {
+	var testTools Tools
+
+	for _, e := range jsonTests {
+		// set the max file size
+		testTools.MaxFileSize = e.maxSize
+
+		// allow / disallow unknown fields
+		testTools.AllowUnknownFields = e.allowUnknown
+
+		// declare a variable to read the decoded JSON into
+		var decodedJSON struct {
+			Foo string `json:"foo"`
+		}
+
+		// create a request with the body
+		req, err := http.NewRequest("POST", "/", bytes.NewReader([]byte(e.json)))
+		if err != nil {
+			t.Error(err)
+		}
+
+		// create a recorder
+		rr := httptest.NewRecorder()
+
+		err = testTools.ReadJSON(rr, req, &decodedJSON)
+
+		if e.errorExpected && err == nil {
+			t.Errorf("%s: error expected but got none", e.name)
+		}
+
+		if !e.errorExpected && err != nil {
+			t.Errorf("%s: error not expected but got one", e.name)
+		}
+
+		if e.errorExpected && err != nil {
+			if !strings.Contains(err.Error(), e.errorMessage) {
+				t.Errorf("%s: error message not expected, got %s", e.name, err.Error())
+			}
+		}
+
+		if !e.errorExpected && err == nil {
+			// if there is no error, check the decoded data
+			if decodedJSON.Foo != "bar" {
+				t.Errorf("%s: expected foo to be bar, got %s", e.name, decodedJSON.Foo)
+			}
+		}
+
+		// check for an error when I try to read from the response body
+		_, err = ioutil.ReadAll(rr.Result().Body)
+		if err != nil {
+			t.Error(err)
+		}
+
+		// reset the max file size
+		testTools.MaxFileSize = 0
+
+		// reset the allowUnknownFields
+		testTools.AllowUnknownFields = false
+	}
+}
+
+func TestTools_WriteJSON(t *testing.T) {
+	var testTools Tools
+
+	rr := httptest.NewRecorder()
+	payload := JSONResponse{
+		Error:   false,
+		Message: "foo",
+	}
+
+	headers := make(http.Header)
+	headers.Add("FOO", "BAR")
+
+	err := testTools.WriteJSON(rr, http.StatusOK, payload, headers)
+	if err != nil {
+		t.Errorf("failed to write JSON: %v", err)
+	}
+}
+
+func TestTools_ErrorJSON(t *testing.T) {
+	var testTools Tools
+
+	rr := httptest.NewRecorder()
+	err := testTools.ErrorJSON(rr, errors.New("some error"), http.StatusUnprocessableEntity)
+	if err != nil {
+		t.Errorf("failed to write JSON: %v", err)
+	}
+
+	var payload JSONResponse
+	decoder := json.NewDecoder(rr.Body)
+	err = decoder.Decode(&payload)
+
+	if err != nil {
+		t.Errorf("failed to decode JSON: %v", err)
+	}
+
+	if !payload.Error {
+		t.Errorf("expected error, got no error")
+	}
+
+	if rr.Code != http.StatusUnprocessableEntity {
+		t.Errorf("expected status code %d, got %d", http.StatusUnprocessableEntity, rr.Code)
+	}
+}
+
+func TestTools_PushJSONToRemote(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(bytes.NewBuffer([]byte(`{"foo": "bar"}`))),
+			Header:     make(http.Header),
+		}
+	})
+
+	var testTools Tools
+	var foo = struct {
+		Bar string `json:"bar"`
+	}{
+		Bar: "bar",
+	}
+
+	_, _, err := testTools.PushJSONToRemote("http://test.com", foo, client)
+
+	if err != nil {
+		t.Errorf("failed to push JSON: %v", err)
 	}
 }
